f64675bd9bc15d262af70a094341abe37ee6389b
Jakov Sosic <jsosic@gmail.com>
2016-12-27 20:05:34 +0100

    Add new parameters
    - kernel_oplocks, socket_params to main section
    - case sensitive and veto oplock to shares

--- manifests/server.pp
+++ manifests/server.pp
@@ -33,6 +33,8 @@ class samba::server (
   $winbind_nested_groups  = 'yes',
   $ad_user                = 'Administrator',
   $ad_password            = 'dummypass',
+  $kernel_oplocks         = undef,
+  $socket_options         = undef,
   $noops                  = undef,
 ) inherits samba::params {
 
@@ -145,7 +147,10 @@ class samba::server (
     exec { 'join_active_directory_domain' :
       command => "/usr/bin/net ads join -U ${ad_user}%${ad_password}",
       onlyif  => '/usr/bin/net ads testjoin -k 2>&1 | /bin/grep -q "not valid"',
-      require => Exec['generate_krb_ticket'],
+      require => [
+        Exec['generate_krb_ticket'],
+        Concat['/etc/samba/smb.conf'],
+      ],
       notify  => Service['smb', 'winbind'],
     }
   }
